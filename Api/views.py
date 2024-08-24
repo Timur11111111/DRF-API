63a792d2b082446a4515613765f1c090ca1e27e7
Timur11111111 <newmew@tutamail.com>
2024-08-24 19:04:54 +0500

    del okk

--- Api/views.py
+++ Api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,25 +6,25 @@ from .models import people
 from rest_framework.permissions import AllowAny
 
 class peopleAPIView(APIView):
-    queryset = people.objects.all()
-    serializer_class = peopleSerializer
     permission_classes = [AllowAny]
+    
     def get(self, request):
         events = people.objects.all()
         serializer = peopleSerializer(events, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = peopleSerializer(data=request.data, many=True)
+        serializer = peopleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def delete(self, request):
-        event = request.data.get()
-        if not event:
+        ids = request.data.get('ids', [])  # Получаем список ID из запроса
+        if not ids:
             return Response({"error": "No IDs provided."}, status=status.HTTP_400_BAD_REQUEST)
         
-        people.objects.filter(id__in=event).delete
-
-        return Response({"message": "People deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+        # Удаляем объекты с указанными ID
+        deleted_count, _ = people.objects.filter(id__in=ids).delete()
+        
+        return Response({"message": f"{deleted_count} people deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
